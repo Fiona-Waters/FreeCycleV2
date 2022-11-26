571bc8d0276c89f67dbd52cf6cc8acd37f635cfc
Fiona Waters <fiwaters6@gmail.com>
2022-11-26 21:38:52 +0000

    firebase auth - app sign up and login implemented

---
+++ app/src/main/java/ie/wit/myapplication/ui/home/Home.kt
@@ -0,0 +1,92 @@
+package ie.wit.myapplication.ui.home
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseUser
+import ie.wit.myapplication.R
+import ie.wit.myapplication.ui.auth.LoggedInViewModel
+import ie.wit.myapplication.ui.auth.Login
+import ie.wit.myapplication.databinding.ActivityMainBinding
+import ie.wit.myapplication.databinding.NavHeaderMainBinding
+import timber.log.Timber
+
+class Home : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var homeBinding: ActivityMainBinding
+    private lateinit var navHeaderMainBinding: NavHeaderMainBinding
+    // private lateinit var navHeaderBinding: NavHeaderBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var loggedInViewModel: LoggedInViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        homeBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
+        drawerLayout = homeBinding.drawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.addFragment, R.id.listFragment, R.id.aboutFragment), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val navView = homeBinding.navView
+        navView.setupWithNavController(navController)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        loggedInViewModel = ViewModelProvider(this).get(LoggedInViewModel::class.java)
+        loggedInViewModel.liveFirebaseUser.observe(this, Observer { firebaseUser ->
+            if (firebaseUser != null)
+                updateNavHeader(loggedInViewModel.liveFirebaseUser.value!!)
+        })
+
+        loggedInViewModel.loggedOut.observe(this, Observer { loggedout ->
+            if (loggedout) {
+                startActivity(Intent(this, Login::class.java))
+            }
+        })
+
+    }
+
+    private fun updateNavHeader(currentUser: FirebaseUser) {
+        var headerView = homeBinding.navView.getHeaderView(0)
+        navHeaderMainBinding = NavHeaderMainBinding.bind(headerView)
+        navHeaderMainBinding.navHeaderEmail.text = currentUser.email
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    fun signOut(item: MenuItem) {
+        loggedInViewModel.logOut()
+        //Launch Login activity and clear the back stack to stop navigating back to the Home activity
+        val intent = Intent(this, Login::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+}
